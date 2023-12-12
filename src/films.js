49ef5cce0c09de09b3cad334c9587bd537b2bdab
Rafa <infowebs.rafa@gmail.com>
2023-12-12 11:57:25 +0100

    Start Exercise 2

--- src/films.js
+++ src/films.js
@@ -8,8 +8,11 @@ function getAllDirectors(movies) {
 }
 
 // Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
- 
+function getMoviesFromDirector(movies, director) {
+
+  const result = movies.filter (movie => movie.director === director);
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.

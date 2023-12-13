eb790762e2fa29c9e32ba300f422e75f23345163
Rafa <infowebs.rafa@gmail.com>
2023-12-13 12:56:52 +0100

    Start Exercise 5

--- src/films.js
+++ src/films.js
@@ -1,41 +1,63 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(movies) {
-  let directorList = [];
-  directorList = movies.map(movie => movie.director);
 
-  console.log("EXERCICE 1 ->", directorList);
-  return directorList;
+    let directorList = [];
+
+    // Extraer el director   
+    directorList = movies.map(movie => movie.director);
+
+    console.log("EXERCICE 1 ->", directorList);
+    return directorList;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(movies, director) {
-  let moviesDirector = [];
-  moviesDirector = movies.filter(movie => movie.director === director);
-  console.log("EXERCICE 2 ->", moviesDirector);
-  return moviesDirector;
+
+    let moviesDirector = [];
+
+    // Filtramos por el director
+    moviesDirector = movies.filter(movie => movie.director === director);
+    console.log("EXERCICE 2 ->", moviesDirector);
+    return moviesDirector;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(movies, director) {  
-  let moviesAverageOfDirector = 0;
-  moviesDirector = getMoviesFromDirector(movies, director);
-  moviesAverageOfDirector = moviesDirector.reduce((total, movie) => (total + movie.score / moviesDirector.length), 0);
-  return moviesAverageOfDirector;
+function moviesAverageOfDirector(movies, director) { 
+    
+    let moviesAverageOfDirector = 0;
+    
+    //  Extraemos las películas por el director  
+    moviesDirector = getMoviesFromDirector(movies, director);
+
+    // Cáculo de la media de puntuación de las películas
+    moviesAverageOfDirector = moviesDirector.reduce((total, movie) => (total + movie.score / moviesDirector.length), 0);
+    return moviesAverageOfDirector;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(movies) {
-  const moviesSortAlpabetically =  movies.map(movie => movie.title);  
-  const top20Titles = moviesSortAlpabetically.sort();
-  return top20Titles.slice(0, 20);
-  
+
+    // Extraer los títulos
+    const moviesSortAlpabetically =  movies.map(movie => movie.title);
+    
+    // Ordenar alfabéticamente
+    const top20Titles = moviesSortAlpabetically.sort();
+
+    // Devolver las 20 primeras películas
+    return top20Titles.slice(0, 20);     
 }
 
 // Exercise 5: Order by year, ascending
 function orderByYear(movies) {
-  const moviesWithYear = movies.map(movie => ({ year: movie.year, title: movie.title })); // Extraer los años y títulos
-  const moviesOrderByYear = moviesWithYear.sort((a, b) => a.year - b.year);               // Ordenar por año
-  return [moviesOrderByYear[0]];                  // Devolver el array con un solo elemento (el objeto ordenado)
+  
+    // Extraer los años y títulos
+    const moviesWithYear = movies.map(movie => ({ year: movie.year, title: movie.title }));
+  
+    // Ordenar por año de forma ascendente
+    const moviesOrderByYear = moviesWithYear.sort((a, b) => a.year - b.year);
+  
+    // Devolver el array con un solo elemento (el objeto con el año más bajo)
+    return [moviesOrderByYear[0]];
 }
 
 // Exercise 6: Calculate the average of the movies in a category
